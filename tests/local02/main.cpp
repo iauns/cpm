f0fdd8855fdfc6b029fa07b6111bcd325077e93d
James Hughes <jhughes@sci.utah.edu>
2013-11-13 23:41:54 -0700

    Added new multiple module tests.

--- tests/local02/main.cpp
+++ tests/local02/main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include "cpm-test-01/module.hpp"
 #include "module1/module.hpp"
 #include "module2/module.hpp"
 
 int main(int argc, char* av[])
 {
-  std::cout << CPM_TEST_01_NS::test01Function("Direct call:", 12, 51) << std::endl;
   std::cout << CPM_MODULE1_NS::module1Function(67) << std::endl;
   std::cout << CPM_MODULE2_NS::module2Function(67, 91) << std::endl;
   return 0;
---
+++ tests/version-test/repoModule2/module2/module.hpp
@@ -0,0 +1,13 @@
+#ifndef MODULE1_H
+#define MODULE1_H
+
+#include <string>
+
+namespace CPM_MODULE2_NS {
+
+std::string module2Function(int num, int num2);
+
+} // namespace CPM_MODULE2_NS 
+
+#endif
+
